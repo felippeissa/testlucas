12fbb12c2902673dd3a65752646826ed10c19e2e
felippeissa <contato.issafe@gmail.com>
2020-03-02 14:15:02 -0300

    initial commit

---
+++ one/index.js
@@ -0,0 +1,13 @@
+const test = require("./testCases");
+
+function one(array) {
+  var soma1 = 0;
+  var arreio = array.map();
+  for (var a = 0; a < array.length(); a++) {
+    var soma1 = arreio[a] + soma1;
+  }
+
+  return resultado;
+}
+
+test(one);
---
+++ two/index.js
@@ -0,0 +1,21 @@
+const test = require("./testCases");
+
+function two(array) {
+    const [arraio] = array.map();
+    const a;
+    const b;
+    for ( var a = 0 ; a< arraio.length(array); a++ ){
+        for ( var b = a+1 ; b< arraio.length(array); b++ ){
+            if(a === 'N' && b === 'S' || a === 'S' && b === 'N' || a === 'L' && b === 'O' || a === 'O' && b === 'L'  ){
+                var newarraio = ;   
+            
+                } 
+
+        }
+
+    }
+    
+    return arraio;
+}
+
+test(two);
